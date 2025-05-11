3279e48b6ce106a74669b6d8c36871e28d2ae69e
gonzaloinsua <creativogn@gmail.com>
2025-05-11 10:21:37 -0300

    Add files via upload

---
+++ InfartosDB_Consultas.sql
@@ -0,0 +1,38 @@
+-- Elijo el Schema sobre el cual trabajar
+USE DB_Infartos;
+
+-- Se generan consultas para corroborar que las tablas hayan recibido correctamente sus valores
+
+SELECT * FROM Fact_AnalisisCardiacos;
+
+SELECT * FROM Dim_Pacientes;
+
+SELECT * FROM Dim_ActividadFisica;
+
+SELECT * FROM Dim_ConsumoAlcohol;
+
+SELECT * FROM Dim_Dieta;
+
+SELECT * FROM Dim_DolorPecho;
+
+SELECT * FROM Dim_EstadoCivil;
+
+SELECT * FROM Dim_Etnia;
+
+SELECT * FROM Dim_Genero;
+
+SELECT * FROM Dim_LugarResidencia;
+
+SELECT * FROM Dim_NivelEducativo;
+
+SELECT * FROM Dim_NivelEstres;
+
+SELECT * FROM Dim_PendienteSegmentoST;
+
+SELECT * FROM Dim_ResultadoECG;
+
+SELECT * FROM Dim_SituacionLaboral;
+
+SELECT * FROM Dim_Talasemia;
+
+SELECT * FROM Dim_VasosObstruidos;
